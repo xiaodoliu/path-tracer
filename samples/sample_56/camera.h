de5fd67695ba2fac1af51afaab817845a47239e0
Xiaodong Liu <xiaodong98128@outlook.com>
2026-06-20 13:48:39 +0800

    pdf of a list of lights

---
+++ samples/sample_56/camera.h
@@ -0,0 +1,107 @@
+# pragma once
+
+#include "vec3.h"
+#include "ray.h"
+#include "material.h"
+#include "bvh.h"
+#include <cuda_runtime.h>
+
+struct scene_object;
+
+struct camera{
+    point3 origin = point3(0, 0, 0);
+    double focal_length = 1.0;
+    double aspect_ratio = 16.0 / 9.0;
+    int image_width = 800;
+    int image_height = 450;
+    int samples_per_pixel = 100;
+    int sqrt_spp = std::sqrt(samples_per_pixel);
+    double recip_sqrt_spp = 1.0 / sqrt_spp;
+    int max_depth = 50;
+    color background;
+    double vfov = 90.0;
+    point3 lookfrom = point3(0, 0, 0);
+    point3 lookat = point3(0, 0, -1);
+    vec3 vup = vec3(0, 1, 0);
+
+    double defocus_angle = 0.0;
+    double focus_dist = 10.0;
+
+    point3 pixel_sample_start;
+    vec3 viewport_pixel_u_delta;
+    vec3 viewport_pixel_v_delta;
+    vec3 u, v, w;
+    vec3 defocus_disk_u, defocus_disk_v;
+
+    void init(int image_width, int samples_per_pixel, int max_depth,
+        double aspect_ratio, double vfov, 
+        point3 lookfrom, point3 lookat, vec3 vup, double defocus_angle = 0.0, double focus_dist = 10.0, color background = color(0.0, 0.0, 0.0)){
+        this->lookfrom = lookfrom;
+        this->lookat = lookat;
+        this->vup = vup;
+        this->defocus_angle = defocus_angle;
+        this->focus_dist = focus_dist;
+        this->background = background;
+        this->origin = lookfrom;
+        this->focal_length = focus_dist;
+        this->vfov = vfov;
+        this->image_width = image_width;
+        this->samples_per_pixel = samples_per_pixel;
+        this->sqrt_spp = std::sqrt(samples_per_pixel);
+        this->recip_sqrt_spp = 1.0 / sqrt_spp;
+        this->max_depth = max_depth;
+        this->aspect_ratio = aspect_ratio;
+        int image_height = static_cast<int>(image_width / aspect_ratio);
+        this->image_height = (image_height < 1) ? 1 : image_height;
+        double theta = degrees_to_radians(vfov);
+        double viewport_height = 2 * focal_length * std::tan(theta / 2);
+        double viewport_width = viewport_height * static_cast<double>(image_width) / image_height;
+        w = normalize(lookfrom-lookat);
+        u = normalize(cross(vup, w));
+        v = cross(w, u);
+        vec3 viewport_u = viewport_width * u;
+        vec3 viewport_v = -viewport_height * v;
+        this->viewport_pixel_u_delta = viewport_u / image_width;
+        this->viewport_pixel_v_delta = viewport_v / image_height;
+        auto viewport_upper_left = this->origin - focal_length * w - viewport_u / 2 - viewport_v / 2;
+        this->pixel_sample_start  = viewport_upper_left + this->viewport_pixel_u_delta / 2 + this->viewport_pixel_v_delta / 2;
+
+        auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = defocus_radius * u;
+        defocus_disk_v = defocus_radius * v;
+    }
+
+    D point3 defocus_disk_sample(curandState* state){
+        auto random_disk_vector = random_in_unit_disk(state);
+        return origin + random_disk_vector.x() * defocus_disk_u + random_disk_vector.y() * defocus_disk_v;
+    }
+
+
+    D vec3 sample_square_stratified(int s_i, int s_j, curandState* state){
+        auto px = (s_i+random_double(state)) * recip_sqrt_spp - 0.5;
+        auto py = (s_j+random_double(state)) * recip_sqrt_spp - 0.5;
+        return vec3(px, py, 0);
+    }
+
+    D ray get_ray(int i, int j, int s_i, int s_j, curandState* state){
+        // auto pixel_sample = pixel_sample_start + 
+        //     (i+(random_double(state)-0.5)) * viewport_pixel_u_delta + 
+        //     (j+(random_double(state)-0.5)) * viewport_pixel_v_delta;
+        auto offset = sample_square_stratified(s_i, s_j, state);
+        auto pixel_sample = pixel_sample_start + 
+            (i + offset.x()) * viewport_pixel_u_delta +
+            (j + offset.y()) * viewport_pixel_v_delta;
+        auto ray_origin = defocus_angle > 0 ? defocus_disk_sample(state) : origin;
+        auto ray_direction = pixel_sample - ray_origin;
+        auto ray_time = random_double(state);
+        return ray(ray_origin, ray_direction, ray_time);
+    }
+
+    void render(int width, int height,
+        scene_object* host_objects, int num_objects,
+        texture_data* host_textures, int num_textures,
+        material_data* host_materials, int num_materials,
+        bvh_node* host_bvh_nodes, int num_bvh_nodes, int root_node_index,
+        int* prim_indices, int num_prim_indices, int* light_indices = nullptr, int num_lights = 0);
+        
+};
---
+++ samples/sample_56/sphere.h
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "hittable.h"
+#include "ray.h"
+#include "onb.h"
+#include <cassert>
+
+class sphere {
+public:
+    HD sphere() = default;
+    HD sphere(const point3& center, double radius, int material_id): center(center, vec3(0, 0, 0)), radius(radius), material_id(material_id){
+        assert(radius > 0);
+        vec3 rvec = vec3(radius, radius, radius);
+        bbox = aabb(center-rvec, center+rvec);
+    }
+    HD sphere(const point3& center1, const point3& center2, double radius, int material_id): center(center1, center2 - center1), radius(radius), material_id(material_id){
+        assert(radius > 0);
+        vec3 rvec = vec3(radius, radius, radius);
+        aabb box1(center1-rvec, center1+rvec);
+        aabb box2(center2-rvec, center2+rvec);
+        bbox = aabb(box1, box2);
+    }
+    D bool hit(const ray& r, interval ray_t, hit_record& rec) const{
+        auto current_center = center.at(r.time());
+        vec3 oc = current_center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(oc, r.direction());
+        auto c = oc.length_squared() - radius * radius;
+        auto discriminant = h * h - a * c;
+        if(discriminant < 0.0){
+            return false;
+        }
+        auto sqrtd = std::sqrt(discriminant);
+        auto t = (h - sqrtd) / a;
+        if(ray_t.less_than(t)){
+            return false;
+        }
+        if(ray_t.greater_than(t)){
+            t = (h + sqrtd) / a;
+            if(!ray_t.contains(t)){
+                return false;
+            }
+        }
+        rec.t = t;
+        rec.p = r.at(t);
+        vec3 outward_normal = (rec.p - current_center) / radius;
+        rec.set_face_normal(r, outward_normal);
+        get_sphere_uv(outward_normal, rec.u, rec.v);
+        rec.material_id = material_id;
+        return true;
+    }
+
+    HD aabb bounding_box() const { return bbox; }
+
+    D double pdf_value(const point3& origin, const vec3& direction) const {
+        hit_record rec;
+        if(!this->hit(ray(origin, direction), interval(0.001, infinity), rec)) {
+            return 0;
+        }
+        auto dist_squared = (center.at(0) - origin).length_squared();
+        auto cos_theta_max = std::sqrt(1 - radius*radius/dist_squared);
+        auto solid_angle = 2 * pi * (1 - cos_theta_max);
+        return  1 / solid_angle;
+    }
+
+    D vec3 random(const point3& origin, curandState* state) const {
+        vec3 direction = center.at(0) - origin;
+        auto distance_squared = direction.length_squared();
+        onb uvw(direction);
+        return uvw.transform(random_to_sphere(radius, distance_squared, state));
+    }
+
+private:
+    ray center;
+    double radius;
+    int material_id;
+    aabb bbox;
+
+    D static void get_sphere_uv(const point3& p, double& u, double& v){
+        auto theta = std::acos(-p.y());
+        auto phi = std::atan2(-p.z(), p.x()) + pi;
+        u = phi / (2*pi);
+        v = theta / pi;
+    }
+
+    D static vec3 random_to_sphere(double radius, double distance_squared, curandState* state){
+        auto r1 = random_double(state);
+        auto r2 = random_double(state);
+        auto z = 1 - r2 * (1 - std::sqrt(1-radius*radius/distance_squared));
+        auto phi = 2 * pi * r1;
+        auto x = std::cos(phi) * std::sqrt(1-z*z);
+        auto y = std::sin(phi) * std::sqrt(1-z*z);
+        return vec3(x, y, z);
+    }
+
+};
